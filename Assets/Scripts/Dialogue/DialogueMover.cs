b6396fae32fd2a746629c9cce3f42ff04dceb7f9
gonszalito <andryanajona@gmail.com>
2022-10-19 14:32:47 +0700

    feature: overhead positon for dialogue
    Added method to move the positions of dialogue to above head

--- Assets/Scripts/Dialogue/DialogueMover.cs
+++ Assets/Scripts/Dialogue/DialogueMover.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class DialogueMover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject dialogueBox;
+    private Camera cam;
+
+    public DialogueMover(GameObject dialogueBox, Camera cam)
     {
-        
+        this.dialogueBox = dialogueBox;
+        this.cam = cam;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    public void SetDialogueOnTalkingCharacter()
     {
-        
+        GameObject character;
+        // Get the dialogue line
+        // Search the GameObject of the character in the Scene
+        character = GameObject.Find("Player");
+        // Sets the dialogue position
+        SetDialoguePosition(character);
+    }
+
+    private void SetDialoguePosition(GameObject character)
+    {
+        // Retrieve the position where the top part of the sprite is in the world
+        float characterSpriteHeight = character.GetComponent<SpriteRenderer>().sprite.bounds.extents.y;
+
+        // Create position with the sprite top location
+        Vector3 characterPosition = new Vector3(character.transform.position.x,
+                                                characterSpriteHeight,
+                                                character.transform.position.z);
+
+        // Set the DialogueBubble position to the sprite top location in Screen Space
+        this.transform.position = cam.WorldToScreenPoint(characterPosition);
     }
 }
