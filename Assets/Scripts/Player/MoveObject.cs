0a586bd01d76f2f0af4813c25417c4a3307b10f5
gonszalito <andryanajona@gmail.com>
2022-10-21 18:34:44 +0700

    wip: movement when talking halted
    -movement when interacting with NPC is being halted as not used
    -added the quest system for the SandBox scene

---
+++ Assets/Scripts/Player/MoveObject.cs
@@ -0,0 +1,27 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class MoveObject : MonoBehaviour
+// {
+
+//     [SerializeField] Transform[] Position;
+//     // Start is called before the first frame update
+//     private int nextPosIndex;
+//     Transform nextPos;
+//     void Start()
+//     {
+//         NextPos = Position[0];
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+
+//     void MoveGameObject()
+//     {
+//         transform.position = Vector2.MoveTowards()
+//     }
+// }
