dda118f864e03059e81552b8fec9212e8aa4826c
Suhe <58254410+tabbyfox@users.noreply.github.com>
2022-11-10 19:31:52 +0700

    feature: added ending scene and sound bug fix

---
+++ Assets/Scripts/Menu/EndingMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class EndingMenu : MonoBehaviour
+{
+    public GameObject endingFirstButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectedButton(endingFirstButton);
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void PlaySelectSound()
+    {
+        SoundManager.PlaySound("MenuButton");
+    }
+
+    public void PlayHoverSound()
+    {
+        SoundManager.PlaySound("MenuButton_hover");
+    }
+
+    private void SelectedButton(GameObject firstSelected)
+    {
+        // clear selected button
+        EventSystem.current.SetSelectedGameObject(null);
+        // set a new selected button
+        EventSystem.current.SetSelectedGameObject(firstSelected);
+    }
+}
